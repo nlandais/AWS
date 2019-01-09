dbf2416bd202d26bcbe7056ac1e6380780dbdb1c
Nicolas Landais <nicolas.landais@citrix.com>
2019-01-09 18:30:50 -0500

    Use data sources to populate subnets and VPC ids based on tags

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,14 +7,10 @@ variable "vpc_id" {
   default = "vpc-0b50566b59bbaa1ce"
 }
 
-variable "plublic_subnet" {
-  default = "subnet-00d38e0d1a530aee0"
-}
-
-variable "private_subnet" {
-  default = "subnet-06f38a1cdb5b6279e"
-}
-
 variable "myip" {
 #TF_VAR_myip=`dig +short myip.opendns.com @resolver1.opendns.com` terraform plan
+}
+
+variable "vpn_ami_id" {
+  default = "ami-07a8d85046c8ecc99"
 }
